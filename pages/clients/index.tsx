12a8aedafc5813a67bf60f5692fcdb2b4a68319a
shm_usuf <shmusuf@gmail.com>
2023-03-04 00:37:23 +0600

    data fetching from server

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -7,6 +7,65 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Container } from "@mui/system";
+//-------------------------------
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
+
+//------------------------------------
+
+const myobj = [
+  {
+    id: 1,
+    user_id: "189892788390",
+    first_name: "Shm",
+    last_name: "Usuf",
+    email: "as@gmail.com",
+    pass: "$2a$08$t9C8RfYq5fveHuL0T8uO6OfnhEXl0V4U8JlmuhIT8JthVi0hhn7Fa",
+    stripe_customer_id: null,
+    stripe_subscription_id: null,
+    lawyer_categories: null,
+    profile_pic: "https://randomuser.me/api/portraits/men/40.jpg",
+    role: "lawyer",
+    phone: "01711223344",
+    createdAt: "2023-03-02T01:33:55.513Z",
+    updatedAt: "2023-03-02T01:33:55.513Z",
+  },
+  {
+    id: 2,
+    user_id: "18989244788390",
+    first_name: "Shn",
+    last_name: "Usuf",
+    email: "as@gmail.com",
+    pass: "$2a$08$gI/w3UGy1I2cTXnWJhXDFeMdLNhJpHLjWZ98532AWDaaQnh2sinaC",
+    stripe_customer_id: null,
+    stripe_subscription_id: null,
+    lawyer_categories: null,
+    profile_pic: "https://randomuser.me/api/portraits/men/40.jpg",
+    role: "lawyer",
+    phone: "01711223344",
+    createdAt: "2023-03-02T03:10:03.212Z",
+    updatedAt: "2023-03-02T03:10:03.212Z",
+  },
+  {
+    id: 3,
+    user_id: "1898924478558390",
+    first_name: "Shm",
+    last_name: "Usuf",
+    email: "as@gmail.com",
+    pass: "$2a$08$y6Ex4/yOmc5BpA0kJu2Ve.k8NKSxFVV.QVBhUehmcYnDhtgxWOznu",
+    stripe_customer_id: null,
+    stripe_subscription_id: null,
+    lawyer_categories: null,
+    profile_pic: "https://randomuser.me/api/portraits/men/40.jpg",
+    role: "lawyer",
+    phone: "01711223344",
+    createdAt: "2023-03-02T05:51:07.885Z",
+    updatedAt: "2023-03-02T05:51:07.885Z",
+  },
+];
+
+//-------------------------------------
 
 function createData(
   name: string,
@@ -25,9 +84,25 @@ const rows = [
 ];
 
 export default function Clients() {
+  const [data, setData] = useState("");
+  const [error, setError] = useState("");
+
   //getting data from the server
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   //featching data
+  const fetchData = async () => {
+    await axios
+      .get("http://35.77.46.232:8080/admin/service-providers")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
 
   return (
     <Container fixed sx={{ marginTop: "30px" }}>
@@ -36,15 +111,15 @@ export default function Clients() {
           <caption>A basic table example with a caption</caption>
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell align="right">User Name</TableCell>
+              <TableCell align="right">Phone Number</TableCell>
+              <TableCell align="right">Role</TableCell>
+              <TableCell align="right">Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {/* {rows.map((row) => (
               <TableRow key={row.name}>
                 <TableCell component="th" scope="row">
                   {row.name}
@@ -54,11 +129,30 @@ export default function Clients() {
                 <TableCell align="right">{row.carbs}</TableCell>
                 <TableCell align="right">{row.protein}</TableCell>
               </TableRow>
-            ))}
+            ))} */}
+            {myobj &&
+              myobj.map((obj) => (
+                <TableRow key={obj?.id}>
+                  <TableCell component="th" scope="row">
+                    <img
+                      style={{ width: "50px" }}
+                      src={obj?.profile_pic}
+                      alt={obj?.first_name}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    {obj?.first_name} {obj?.last_name}
+                  </TableCell>
+                  <TableCell align="right">{obj?.phone}</TableCell>
+                  <TableCell align="right">{obj?.role}</TableCell>
+                  <TableCell align="right">
+                    <span>details</span> <span>reset</span>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
     </Container>
   );
 }
-//hiii hii
